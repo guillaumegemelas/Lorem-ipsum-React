fd39d07d402eabe9a1c00caaaa76d656bd55b28a
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-01-30 22:42:20 +0100

    fin soirée

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,6 @@
 const Title = (props) => {
   console.log(props);
-  return <h1 className={props.title}> {props.title}</h1>;
+  return <h1 className="titleh1"> {props.title}</h1>;
 };
 
 export default Title;

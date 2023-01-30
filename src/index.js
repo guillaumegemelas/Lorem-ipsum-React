262fb365a24a1de39935bdea03d65d57b233fcdd
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-01-30 12:24:55 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import du fichier reset qu'on l'on a mis dans le index.css
+import "./index.css";
+//import du composant App
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// J'injecte le contenu de l'appel du App dans la div dont l'id est "root"
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +16,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
